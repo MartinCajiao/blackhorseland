4f9e04b857322627768c28a4c97721ebb976db99
Martin Cajiao <patrmartin7@gmail.com>
2026-04-14 15:33:20 -0600

    new

--- blackhorse-landing/astro.config.mjs
+++ blackhorse-landing/astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  // Tu dominio base de GitHub
+  site: "https://martincajiao.github.io",
+
+  // EL NOMBRE EXACTO DE TU REPOSITORIO (asegúrate de que coincida con mayúsculas y minúsculas)
+  base: "/BlackHorse",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
